a17734d6d1d97a6498e2678b7c37cd3a0299416c
Mauro Bringas <maurosbringas@gmail.com>
2020-11-05 15:04:49 -0300

    Se añadió funcioens para añadir y mostrar las notas dentro de cards, botones,iconos y diseño.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const titleInput = document.getElementById('title');
 const textInput = document.getElementById('text');
 const noteForm = document.getElementById('noteForm');
+const categoryInput = document.getElementById('categoryInput')
+const notesAlmacenator = document.getElementById('notesAlmacenator')
 
 const generateId = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
@@ -12,16 +14,40 @@ noteForm.onsubmit = (e) => {
     const notes = JSON.parse(localStorage.getItem('notes')) || [];
     const title = titleInput.value;
     const text = textInput.value;
-    
-    notes.push ({
+    const category = categoryInput.value;
+
+    notes.push({
         title: title,
         text: text,
         id: generateId(),
+        category: category,
         createdAt: Date.now()
     })
     localStorage.setItem('notes', JSON.stringify(notes))
     console.log(notes);
     noteForm.reset();
 
-
+    displayNote()
 }
+function displayNote() {
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    const rows = []
+    for (let i = 0; i < notes.length; i++) {
+        const note = notes[i];
+        const card = `
+                <div class="card" style="width: 18rem; background: #191b1de5">
+                    <div class="card-body">
+                        <h5 class="card-title">${note.title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${note.category}</h6>
+                        <p class="card-text">${note.text}</p>
+                        <button class="btn btn-danger text-white"><i class="fas fa-trash-alt"></i></button>
+                        <button class="btn btn-warning text-white"><i class="fas fa-pencil-alt"></i></button>    
+                    </div>
+                </div>
+                `
+        rows.push(card)
+    }
+    notesAlmacenator.innerHTML = rows.join('')
+}
+
+displayNote()
